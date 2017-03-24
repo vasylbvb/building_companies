89ecac2b4a50182d3080751b9d4fb4fe644e98d5
Vasyl Bats <basebv@gmail.com>
2017-03-24 09:25:42 +0200

    finished home page

--- src/js/main.js
+++ src/js/main.js
@@ -62,10 +62,18 @@ function myMap() {
     var mapCanvas = document.getElementById("map");
     var myCenter = new google.maps.LatLng(49.5512725, 25.6156704);
     var mapOptions = {center: myCenter, zoom: 17};
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var map = new google.maps.Map(mapCanvas, mapOptions, {
+        scrollwheel: false,
+    });
     var marker = new google.maps.Marker({
         position: myCenter,
         animation: google.maps.Animation.BOUNCE
+    });
+    google.maps.event.addListener(map, 'mouseout', function(){
+        this.setOptions({scrollwheel:false});
+    });
+    map.addListener('click', function() {
+        map.set('scrollwheel', true);
     });
     marker.setMap(map);
 }
